5fa6fd49bcb6b2642c82765e810821616382d720
Toby Neal <tobyneal92@gmail.com>
2022-01-27 09:23:17 +0000

    changed backspace to arrow icon

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -30,7 +30,10 @@ export const Keyboard = ({ correctLetters, outOfPositionLetters, incorrectLetter
           "Q W E R T Y U I O P",
           "A S D F G H J K L",
           "Enter Z X C V B N M {bksp}",
-        ]
+        ],
+      }}
+      display={{
+        "{bksp}": "⌫",
       }}
       buttonTheme={[
         { 
